0fd6a11bf3726a2f891715a0078d926df191f14a
nickvirag <nick@nickv.co>
2019-12-01 14:46:28 -0500

    Create production build with webpack and babel

--- src/api/bufferConversion.js
+++ src/api/bufferConversion.js
@@ -69,7 +69,7 @@ export const filepathsToBuffer = (filepaths) => {
     base64 += base64Chunk;
   });
 
-  const buffer = new Buffer(base64, 'base64');
+  const buffer = Buffer.from(base64, 'base64');
 
   return buffer;
 };
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+  },
+  target: 'node',
+};
